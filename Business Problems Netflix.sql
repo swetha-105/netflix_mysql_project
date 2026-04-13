064b58c0c10d34f4e8bb687e42dcceb87599b08c
KOTALA SWETHA <146550477+swetha-105@users.noreply.github.com>
2026-04-13 23:30:03 +0530

    Update Business Problems Netflix.sql

--- Business Problems Netflix.sql
+++ Business Problems Netflix.sql
@@ -3,21 +3,18 @@
 1. Count the number of Movies vs TV Shows
 2. Find the total number of content items for each rating within each content type and sort them by highest count.
 3.  Retrieve all content released in the year 2020 from the Netflix dataset.
-4. Find the top 5 countries with the most content on Netflix
-5. Identify the longest movie
-6. Find content added in the last 5 years
-7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
-8. List all TV shows with more than 5 seasons
-9. Count the number of content items in each genre
-10.Find each year and the average numbers of content release in India on netflix. 
-return top 5 year with highest avg content release!
-11. List all movies that are documentaries
-12. Find all content without a director
-13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
-14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
-15.Categorize the content based on the presence of the keywords 'kill' and 'violence' in the 
-description field. Label content containing these keywords as 'Bad' and all other content as 'Good'. 
-Count how many items fall into each category.
-16.List oldest movies on netflix.
-17.Titles containing a specific keyword "Love".
-18.List top 5 countries producing TV shows vs Movies.
+4. Retrieve the top 5 countries with the highest number of content items in the dataset.
+5. Find the movie with the maximum duration from the dataset.
+6.Retrieve all content that was added to Netflix in the last 5 years.
+7. Retrieve all content directed by 'Rajiv Chilaka' from the dataset.
+8. Retrieve all TV shows that have more than 5 seasons.
+9. Count the number of content items in each genre category as listed in the dataset.
+10. Find the number of content items released each year in India and return the top 5 years with the highest count.
+11. Retrieve all content that belongs to the ‘Documentaries’ category.
+12. Retrieve all content that does not have a director specified.
+13. Find the total number of movies featuring actor ‘Salman Khan’ released in the last 10 years.
+14. Find the top 10 cast entries with the highest number of content items produced in India.
+15. Classify content as ‘Bad’ or ‘Good’ based on keywords in the description and count the number of items in each category.
+16.Retrieve the top 10 oldest movies based on release year from the dataset.
+17.Retrieve all content titles that contain the keyword ‘Love’.
+18. Retrieve the number of content items for each country and content type, and display the top results based on count.
